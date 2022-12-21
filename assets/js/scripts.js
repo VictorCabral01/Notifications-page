a96d9b32c5dfc60ff510ae92645b8a9e40570f3b
VictorPaiva01 <victor_paiva@hotmail.com>
2022-12-20 21:32:28 -0300

    Alteração do Repositorio

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+
+const click = document.getElementById('teste').style.backgraund = 'red'
+
+click.addEventListener("Click", eventoClick);
+
+
+
+function eventoClick(){
+
+const click = document.getElementById('teste')
+      
+
+click.classList.add("cinza");
+
+   
+}
+
