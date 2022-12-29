572b748e829726c7fbf57e6570ed5ad0ebf34dbd
VictorPaiva01 <victor_paiva@hotmail.com>
2022-12-29 13:05:04 -0300

    Atualização do Projeto

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,9 +3,29 @@
 function eventoClick(x){
 
  
-document.getElementById(x).style.background = "rgb(243, 236, 236)"; 
+const cinza = document.getElementById(x).style.background = "rgb(243, 236, 236)"; 
 
-  
+
+
 }
 
+const cicle = document.getElementsByClassName('ClickRed')
 
+cicle.addEventoListen("Click", clickRed )
+
+function clickRed (){
+
+    const cicle = document.querySelector('.ClickRed').style.background = "red"
+      
+
+    
+
+}
+
+function evento(y){
+
+ 
+    const branco = document.getElementById(y).style.background = "white"; 
+    
+    
+}
